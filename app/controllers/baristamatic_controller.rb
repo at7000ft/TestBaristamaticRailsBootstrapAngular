a3534fb014988aa7d58ec9b09a3cd0e97ff4b263
rholl00 <rick.holland@bhnetwork.com>
2015-01-11 13:31:27 -0700

    Doc update

--- app/controllers/baristamatic_controller.rb
+++ app/controllers/baristamatic_controller.rb
@@ -1,6 +1,6 @@
 #
 # Title: BaristaController
-# Description: A pass through controller loading single index.html.erb page.
+# Description: A pass through controller loading single index.html.erb page. Access at http://localhost:3000.
 #
 # Author: rholl00
 # Date: 1/7/15
